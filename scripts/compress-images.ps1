9af2a2fc59d19b0d29dfb1e110cc34c2e859d5d7
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2024-02-01 02:49:37 +0200

    Edited image styling to make them more card-like and adjusted compress-images script

--- scripts/compress-images.ps1
+++ scripts/compress-images.ps1
@@ -9,11 +9,20 @@ foreach ($ImageFile in $ImageFiles) {
     # Define the output file name
     $OutputFile = $ImageFile.FullName -replace '\.[^.]*$', '.webp'
     
-    # Define the ImageMagick command to run on the current image
-    $ImageMagickCommand = "magick convert `"$ImageFile`" -strip -resize 1280x1920 -define webp:lossless=false -quality 85 -define webp:alpha-quality=80 -define webp:auto-filter=true -define webp:method=6 `"$OutputFile`""
+    # Define the ImageMagick command to run on the current image for lossless compression
+    $ImageMagickCommandLossless = "magick convert `"$ImageFile`" -strip -adaptive-resize 1200x1920 -define webp:lossless=true `"$OutputFile`""
     
-    # Run the ImageMagick command on the file
-    Invoke-Expression -Command $ImageMagickCommand
+    # Run the ImageMagick command on the file for lossless compression
+    Invoke-Expression -Command $ImageMagickCommandLossless
+
+    # Check the file size of the output file
+    $OutputFileSize = (Get-Item $OutputFile).Length / 1KB
+
+    # If the file size is larger than 60KB, compress it lossily
+    if ($OutputFileSize -gt 100) {
+        $ImageMagickCommandLossy = "magick convert `"$ImageFile`" -strip -adaptive-resize 1200x1920 -define webp:lossless=false -quality 85 -define webp:alpha-quality=80 -define webp:auto-filter=true -define webp:method=6 `"$OutputFile`""
+        Invoke-Expression -Command $ImageMagickCommandLossy
+    }
 
     # Delete the original image
     Remove-Item -Path $ImageFile.FullName
